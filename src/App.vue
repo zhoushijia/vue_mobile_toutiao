3b9eb4c0ac1dcdd141772a1b3494827cdd192827
zhoushijia <zhoushijia@foxmail.com>
2021-02-23 15:13:54 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h1>dadada</h1>
     <i class="toutiao toutiao-dianzan"></i>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
