fb98db25c45b109420040f0d8703f5a6f52e4a59
zhoushijia <zhoushijia@foxmail.com>
2021-02-27 10:19:47 +0800

    频道列表和汉堡导航布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,118 @@
 <template>
-  <div>首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" fixed>
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+        >搜索</van-button
+      >
+    </van-nav-bar>
+
+    <!-- 频道列表 -->
+    <van-tabs
+      class="channel-tabs"
+      v-model="activeName"
+      animated
+      swipeable
+      swipe-threshold="3"
+    >
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="toutiao toutiao-gengduo"></i>
+      </div>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIndex'
+  name: 'HomeIndex',
+  data() {
+    return {
+      activeName: 0
+    }
+  }
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.home-container {
+  padding-top: 92px;
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+  /deep/ .channel-tabs {
+    .van-tab {
+      border-right: 1px solid #edeff3;
+      min-width: 200px;
+      height: 80px;
+      .van-tab__text {
+        font-size: 27px;
+        color: #777777;
+      }
+    }
+
+    .van-tab--active .van-tab__text {
+      color: #333 !important;
+    }
+    .van-tabs__line {
+      width: 31px !important;
+      height: 6px;
+      background: rgba(50, 150, 250, 1);
+      border-radius: 3px;
+      bottom: 8px;
+    }
+    .van-tabs__nav {
+      padding: 0;
+    }
+    .placeholder {
+      flex-shrink: 0;
+      width: 66px;
+      height: 82px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      opacity: 0.902;
+      i.toutiao {
+        font-size: 33px;
+      }
+      &:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 100%;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
+    }
+  }
+}
+</style>
