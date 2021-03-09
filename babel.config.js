e9a56903f963b80263682d9af214cf91ce4cfc31
zhoushijia <zhoushijia@foxmail.com>
2021-03-09 21:11:12 +0800

    上线时去除log

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
+// 项目发布阶段需要用的插件
+const prodPlugins = []
+
+// 上线时 去除log
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+  presets: ['@vue/cli-plugin-babel/preset'],
+  plugins: [...prodPlugins]
 }
